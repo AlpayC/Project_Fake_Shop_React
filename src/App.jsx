2b533c12e8e47e8644bb4a6940fe0f1f2044897b
AlpayC <alpaycelik91@googlemail.com>
2023-06-07 22:35:23 +0200

    updated app.jsx base url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <BrowserRouter
-        basename={import.meta.env.DEV ? "/" : "/react-vite-gh-pages/"}
+        basename={import.meta.env.DEV ? "/" : "/Project_Fake_Shop_React/"}
       >
         <Routes>
           <Route path="/" element={<Home />} />
