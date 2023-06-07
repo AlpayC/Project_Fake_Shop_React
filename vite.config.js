3a2e3ebeef604025f83f16b03a7058632040f694
AlpayC <alpaycelik91@googlemail.com>
2023-06-07 22:40:10 +0200

    added conditional path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/Project_Fake_Shop_React/";
+  }
+
+  return config;
 });
